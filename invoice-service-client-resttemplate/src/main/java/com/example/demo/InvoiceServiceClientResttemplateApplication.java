2cb284a6c0e75288c3a7e8f866288c81afeb92c0
Nikhil Ganage <Nikhil Ganage@Nikhil>
2021-04-18 18:11:43 +0530

    Create Invoice Service Client with Rest Template

---
+++ invoice-service-client-resttemplate/src/main/java/com/example/demo/InvoiceServiceClientResttemplateApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class InvoiceServiceClientResttemplateApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(InvoiceServiceClientResttemplateApplication.class, args);
+		
+		RestTemplate template = context.getBean(RestTemplate.class);
+		
+		String response = template.getForObject("http://localhost:6060/api/v1/invoices", String.class);
+		
+		System.out.println(response);
+	}
+	
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
+	}
+
+}
