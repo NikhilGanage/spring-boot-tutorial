bcb1b67fcf204e8d2c8a29b450041b549f6e6dc8
Nikhil Ganage <Nikhil Ganage@Nikhil>
2021-04-18 17:35:06 +0530

    Create Customer Service Hateos project

---
+++ customer-service-hateos/src/main/java/com/example/demo/CustomerServiceHateosApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+
+@SpringBootApplication
+public class CustomerServiceHateosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceHateosApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			CustomerRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+		
+				repo.save(new Customer(1,"John",112233));
+				repo.save(new Customer(2,"Harry",889900));
+				
+			}
+		};
+	}
+
+}
